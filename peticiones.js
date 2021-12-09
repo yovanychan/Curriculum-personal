491d4c94eb839e99aced5aac256f8efbc7bf7968
yovanychan <miguel.chan@uimqroo.edu.mx>
2021-12-09 18:57:17 -0500

    codigo

--- peticiones.js
+++ peticiones.js
@@ -1,20 +1,20 @@
-const express = require ('express')
+const express = require('express')
 const app = express()
 const port = 3000
 
 app.set('view engine','ejs')// motor de plantillas
 app.set('views',__dirname + '/views') // para invocar la ubicación del archivo views
 
-app.use(express.static(__dirname + "/public"))
+app.use(static(__dirname + "/public" ))
 
 app.get('/',(req,res)=>{
     res.render("index",{
-        titulo: "Página de inicio"
+        titulo : "Estamos en el Inicio"
     })
 })
 
 
 
-app.listen(port, () =>{
-    console.log('Servidor',port)
-})
+app.listen(port,()=>{
+    console.log('Servidor OK, en el puerto',port)
+}) 

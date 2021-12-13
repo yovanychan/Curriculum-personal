e99de7d5e0500c0ccdc99ee8246d155bff1c2a64
yovanychan <miguel.chan@uimqroo.edu.mx>
2021-12-13 16:06:34 -0500

    proyecto

--- peticiones.js
+++ peticiones.js
@@ -5,16 +5,50 @@ const port = 3000
 app.set('view engine','ejs')// motor de plantillas
 app.set('views',__dirname + '/views') // para invocar la ubicación del archivo views
 
-app.use(static(__dirname + "/public" ))
+app.use(express.static(__dirname + "/public" ))
 
 app.get('/',(req,res)=>{
     res.render("index",{
-        titulo : "Estamos en el Inicio"
+        titulo : "Principal"
     })
 })
 
+app.get('/datosPersonales',(req,res)=>{
+    res.render("datosPersonales",{
+        titulo : "Datos personales"
+    })
+})
 
+app.get('/contactanos',(req,res)=>{
+    res.render("contactanos",{
+        titulo : "Contáctanos"
+    })
+})
+
+app.get('/niveleEstudio',(req,res)=>{
+    res.render("niveleEstudio",{
+        titulo : "Nivel de estudio"
+    })
+})
+
+app.get('/ExperienciaLaboral',(req,res)=>{
+    res.render("ExperienciaLaboral",{
+        titulo : "Experiencia laboral"
+    })
+})
+
+app.get('/idiomaDomina',(req,res)=>{
+    res.render("idiomaDomina",{
+        titulo: "Idiomas que se dominan"
+    })
+})
+
+app.get('/equipoDomina',(req,res)=>{
+    res.render("equipoDomina",{
+        titulo: "Equipos que se dominan"
+    })
+})
 
 app.listen(port,()=>{
-    console.log('Servidor OK, en el puerto',port)
+    console.log('Servidor',port)
 }) 
